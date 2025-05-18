7e6020f75a185832f74363ee605a4bfe965064f8
JM <pissanu.klo@gmmmusic.com>
2025-05-18 12:25:19 +0700

    add log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  Logger.log('Nest application started on port 3000', 'NestApplication');
 }
 bootstrap();
